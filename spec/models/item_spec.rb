595027def56e7dd87d57d9a6e4b7c453a4f69d52
Tonio Serna <antonelo@gmail.com>
2021-05-12 12:15:22 +0200

    Add test cases via TDD and fix versioning rspec issue from v3 to v4

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+# `name` is not available from within an example (e.g. an `it` block) or fro
+# constructs that run in the scope of an example (e.g. `before`, `let`, etc).
+# It is only available on an example group (e.g. a `describe` or `context` block)
+
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure an item record belongs to a single todo record
+  it { should belong_to(:todo) }
+  # Validation test
+  # ensure column name is present before saving
+  it { should validate_presence_of(:name) }
 end
