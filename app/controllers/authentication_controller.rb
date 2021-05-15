36b1dbd5de6896ed57b5916ebe2bce824b340b63
Tonio Serna <antonelo@gmail.com>
2021-05-15 12:36:55 +0200

    Add authentication controller via TDD  and specific routes

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# expose an /auth/login endpoint that accepts user credentials
+# also returns a JSON response with the result.
+class AuthenticationController < ApplicationController
+  # return auth token once user is authenticated
+  def authenticate
+    auth_token =
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  private
+
+  def auth_params
+    params.permit(:email, :password)
+  end
+end
